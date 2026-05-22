6a5d62b19a2b265460ca6900e62b7970b3e53f69
L3andro-Just1no <leandrojustino2025@gmail.com>
2026-05-22 17:00:44 +0100

    carousel

---
+++ src/lib/assets/partners.ts
@@ -0,0 +1,22 @@
+const base = "/images/partners";
+
+/** Order matches zion-creativeartisans.com partner carousel */
+export const partnerLogos = [
+  { id: "ustoa", image: `${base}/ustoa.png`, alt: "USTOA", width: 220, height: 220 },
+  { id: "asta", image: `${base}/asta.png`, alt: "ASTA", width: 227, height: 227 },
+  { id: "savoy", image: `${base}/savoy.png`, alt: "Savoy Signature", width: 233, height: 233 },
+  { id: "vila-vita-parc", image: `${base}/vila-vita-parc.png`, alt: "Vila Vita Parc", width: 224, height: 224 },
+  { id: "torel-boutiques", image: `${base}/torel-boutiques.png`, alt: "Torel Boutiques", width: 220, height: 220 },
+  { id: "sao-lourenco", image: `${base}/sao-lourenco.png`, alt: "São Lourenço", width: 220, height: 220 },
+  { id: "lifestyle-portugal", image: `${base}/lifestyle-portugal.png`, alt: "Lifestyle Portugal", width: 220, height: 220 },
+  { id: "bela-vista", image: `${base}/bela-vista.png`, alt: "Bela Vista Hotel & Spa", width: 220, height: 220 },
+  { id: "jardins-do-lago-1", image: `${base}/jardins-do-lago-1.png`, alt: "Quinta Jardins do Lago", width: 220, height: 220 },
+  { id: "jardins-do-lago", image: `${base}/jardins-do-lago.png`, alt: "Quinta Jardins do Lago", width: 220, height: 220 },
+  { id: "octant", image: `${base}/octant.png`, alt: "Octant Hotels", width: 220, height: 220 },
+  { id: "vila-foz", image: `${base}/vila-foz.png`, alt: "Vila Foz Hotel & Spa", width: 220, height: 220 },
+  { id: "conrad", image: `${base}/conrad.png`, alt: "Conrad", width: 220, height: 220 },
+  { id: "iltm", image: `${base}/iltm.png`, alt: "ILTM", width: 220, height: 220 },
+  { id: "iltm-cannes", image: `${base}/iltm-cannes.png`, alt: "ILTM Cannes", width: 220, height: 220 },
+  { id: "le-miami", image: `${base}/le-miami.png`, alt: "LE Miami", width: 220, height: 220 },
+  { id: "connections-luxury", image: `${base}/connections-luxury.png`, alt: "Connections Luxury", width: 220, height: 220 },
+] as const;
